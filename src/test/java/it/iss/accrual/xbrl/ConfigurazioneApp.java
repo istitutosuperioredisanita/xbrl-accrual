208f2cf054aefea30dd7ef5f5beced4d6a30a963
csalvio <ciro.salvio@iss.it>
2026-06-18 13:45:34 +0200

    Removed the "Spring" framework

--- src/test/java/it/iss/accrual/xbrl/ConfigurazioneApp.java
+++
@@ -1,9 +0,0 @@
-package it.iss.accrual.xbrl;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "it.iss.accrual.xbrl") // S
-public class ConfigurazioneApp {
-}
